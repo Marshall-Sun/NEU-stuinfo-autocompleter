80163d497f46ad3b0ccb5e117fde3a4ae8f63d42
Marshall-Sun <910204019@qq.com>
2020-05-31 22:48:59 +0800

    Update NEU-stuinfo-autocompleter.user.js

--- NEU-stuinfo-autocompleter.user.js
+++ NEU-stuinfo-autocompleter.user.js
@@ -2,25 +2,37 @@
 // @name         NEU-stuinfo-autocompleter
 // @namespace    https://e-report.neu.edu.cn/
 // @description  东北大学师生健康信息上报平台的自动填写，自动提交
-// @version      1.1
+// @version      1.4
 // @author       Marshall Sun
 // @match        https://e-report.neu.edu.cn/notes/create
+// @match        https://e-report.neu.edu.cn/report/forms/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var r = confirm(
+    if(window.location.href.substr(0,33)=="https://e-report.neu.edu.cn/notes") {
+        var r = confirm(
         "请确认你满足下列所有条件：\n\n" +
         "1.本次为本人上报\n" +
         "2.目前身体状况正常\n" +
         "3.所处位置无变化\n" +
         "\n你是否确认？"
         );
-    if (r == true) {
-        document.getElementsByName('jibenxinxi_shifoubenrenshangbao')[0].click();
-        document.getElementsByName('jiankangxinxi_muqianshentizhuangkuang')[0].click();
-        setTimeout("document.getElementsByName('xingchengxinxi_weizhishifouyoubianhua')[1].click()",500);
-        setTimeout("document.getElementsByTagName('button')[1].click()",600);
+        if (r == true) {
+            document.getElementsByName('jibenxinxi_shifoubenrenshangbao')[0].click();
+            document.getElementsByName('jiankangxinxi_muqianshentizhuangkuang')[0].click();
+            setTimeout("document.getElementsByName('xingchengxinxi_weizhishifouyoubianhua')[1].click()",900);
+            setTimeout("document.getElementsByTagName('button')[2].click()",1000);
+        }
+    }
+
+    if(window.location.href.substr(0,33)=="https://e-report.neu.edu.cn/repor") {
+        document.getElementsByTagName('input')[1].click();
+        document.getElementsByTagName('input')[3].click();
+        document.getElementsByTagName('input')[5].click();
+        document.getElementsByTagName('input')[7].click();
+        document.getElementsByTagName('input')[9].click();
+        setTimeout("document.getElementsByTagName('button')[0].click();",500);
     }
 })();
