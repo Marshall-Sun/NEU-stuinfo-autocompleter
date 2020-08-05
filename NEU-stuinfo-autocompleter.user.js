4d911e4abec71fc0561a593c7ca92093c34237c0
Marshall-Sun <910204019@qq.com>
2020-08-05 22:21:41 +0800

    del unused website

--- NEU-stuinfo-autocompleter.user.js
+++ NEU-stuinfo-autocompleter.user.js
@@ -5,7 +5,6 @@
 // @version      1.7.1
 // @author       Marshall Sun
 // @match        https://e-report.neu.edu.cn/notes/create
-// @match        https://e-report.neu.edu.cn/report/forms/*
 // @grant        none
 // ==/UserScript==
 
@@ -25,14 +24,5 @@
       setTimeout(() => (document.getElementsByName("xingchengxinxi_weizhishifouyoubianhua")[1].click()), 900);
       setTimeout(() => (document.getElementsByTagName("button")[2].click()), 1000);
     }
-  }
-
-  if (window.location.href.substr(0, 34) == "https://e-report.neu.edu.cn/report") {
-    document.getElementsByTagName("input")[1].click();
-    document.getElementsByTagName("input")[3].click();
-    document.getElementsByTagName("input")[5].click();
-    document.getElementsByTagName("input")[7].click();
-    document.getElementsByTagName("input")[9].click();
-    setTimeout(() => (document.getElementsByTagName("button")[0].click()), 10);
   }
 })();
